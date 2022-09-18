2460cf6d3b0c00954fdab0bc401da2f5dc7dba27
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-09-18 08:47:06 +0300

    Add Translator

--- Sources/AzureTranslator/AzureTranslator.swift
+++ Sources/AzureTranslator/AzureTranslator.swift
@@ -1,1 +1,82 @@
-public struct Translator {}
+import Foundation
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
+
+public struct Translator {
+    private let apiKey: String
+    private let region: String?
+    private let session = URLSession.shared
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+
+    public init(apiKey: String, region: String? = nil) {
+        self.apiKey = apiKey
+        self.region = region
+    }
+
+    private func makeURL(path: String, query: [(String, String)] = []) throws -> URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.cognitive.microsofttranslator.com"
+        components.path = path
+        components.queryItems = [
+            URLQueryItem(name: "api-version", value: "3.0"),
+        ] + query.map {
+            URLQueryItem(name: $0.0, value: $0.1)
+        }
+        guard let url = components.url else {
+            throw URLError(.badURL)
+        }
+        return url
+    }
+
+    private func setHeaders(_ request: inout URLRequest) {
+        request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        request.setValue(apiKey, forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
+        if let region = region {
+            request.setValue(region, forHTTPHeaderField: "Ocp-Apim-Subscription-Region")
+        }
+    }
+
+    private func setHTTPBody(_ request: inout URLRequest, _ texts: [String]) throws {
+        struct Element: Encodable {
+            let Text: String
+        }
+        let body = texts.map {
+            Element(Text: $0)
+        }
+        request.httpBody = try encoder.encode(body)
+    }
+
+    private func decodedData<Output: Decodable>(for request: URLRequest) async throws -> Output {
+        let data = try await session.data(for: request)
+        return try decoder.decode(Output.self, from: data)
+    }
+}
+
+private extension URLSession {
+    func data(for request: URLRequest) async throws -> Data {
+        var task: URLSessionDataTask?
+        let onCancel = {
+            task?.cancel()
+        }
+        return try await withTaskCancellationHandler(
+            handler: {
+                onCancel()
+            },
+            operation: {
+                try await withCheckedThrowingContinuation { continuation in
+                    task = dataTask(with: request) { data, _, error in
+                        guard let data = data else {
+                            let error = error ?? URLError(.badServerResponse)
+                            return continuation.resume(throwing: error)
+                        }
+                        continuation.resume(returning: data)
+                    }
+                    task?.resume()
+                }
+            }
+        )
+    }
+}
